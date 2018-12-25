805ecebe4cedba4da19c2e1cc0b9ef36862e7959
zhangweikai <tju_zhangweikai@163.com>
2018-12-26 06:33:33 +0800

    first commit

---
+++ src/main/java/com/spring/inaction/InactionApplication.java
@@ -0,0 +1,14 @@
+package com.spring.inaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InactionApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/spring/inaction/InactionApplicationTests.java
@@ -0,0 +1,17 @@
+package com.spring.inaction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InactionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
